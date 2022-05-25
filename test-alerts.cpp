db20e3888ff5c646f4aaf204f3a75d8ca1770f1a
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-05-25 07:37:20 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -6,4 +6,9 @@
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
   REQUIRE(inferBreach(40, 20, 30) == TOO_HIGH);
+  REQUIRE(inferBreach(25, 20, 30) == NORMAL);
+}
+
+TEST_CASE("classify temp according to limits") {
+REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1)== TOO_LOW); 
 }
